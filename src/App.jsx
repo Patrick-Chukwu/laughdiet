ae550474f24ebe3db29c8b1c6e7bf05e0babf943
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-10 15:45:53 +0100

    feat: initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
-    <div className='text text-primary'>App</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      <Footer/>
+    </div>
   )
 }
 
